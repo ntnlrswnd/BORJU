df92de3381ed4cf1384bed90ada31a8804e4ff9e
ntnlrswnd <natanaeldidi31@gmail.com>
2024-12-03 00:57:32 +0700

    Initial commit

---
+++ app/src/main/java/com/borju/Login.java
@@ -0,0 +1,32 @@
+package com.borju;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void openDashboard(View view) {
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
+    }
+
+
+    public void openRegister(View view) {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+}
