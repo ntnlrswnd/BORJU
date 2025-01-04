4ca16e7c7cb3ba7cfc6d81bc8f8642c9db5c8bb7
Ricco pandora <riccopandora1@gmail.com>
2025-01-04 22:40:39 +0700

    Tambah Pesanan

---
+++ app/src/main/java/com/borju/Pesanan.java
@@ -0,0 +1,22 @@
+package com.borju;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Pesanan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+    }
+    public void openLogin(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+}
